0d4cc46d75ad6f5693775c2cf8c79d25e9452c4a
Sand-jrd <47563242+Sand-jrd@users.noreply.github.com>
2019-09-24 12:49:36 +0200

    Add files via upload

--- Map_interractive/donneesEtps.js
+++ Map_interractive/donneesEtps.js
@@ -2,16 +2,18 @@
 
 
 
-var nb_etp=4;
+var nb_etp=9;
 
 var Etp1 ={
-	nom: 'thales',
+	nom: 'Spie',
+	lien: 'https://www.ingenib.fr/students/catalogue/diginext',
 	filiereRecherchee: ['telecom','info'],
 	activite: ['aero'],
 	ipb: p1     //ici est stoké la balise d'id p1
 };
 var Etp2 ={
-	nom: 'CEA',
+	nom: 'Ingelliance',
+	lien: 'https://www.ingenib.fr/students/catalogue/diginext',
 	filiereRecherchee: ['info'],
 	activite: ['aero','recherche'],
 	ipb: p2
@@ -19,16 +21,58 @@ var Etp2 ={
 
 var Etp3 ={
 	nom: 'Meretis',
+	lien: 'https://www.ingenib.fr/students/catalogue/diginext',
 	filiereRecherchee: ['elec','info'],
 	activite: ['sante'],
 	ipb: p3
 };
 
 var Etp4 ={
-	nom: 'capgemini',
+	nom: 'Qucit',
+	lien: 'https://www.ingenib.fr/students/catalogue/diginext',
 	filiereRecherchee: ['telecom','info','mmk'],
 	activite: ['sante','robot'],
 	ipb: p4
 };
 
-listeEtp=[Etp1,Etp2,Etp3,Etp4];
+var Etp5 ={
+	nom: 'Tehtis',
+	lien: 'https://www.ingenib.fr/students/catalogue/diginext',
+	filiereRecherchee: ['mmk','elec'],
+	activite: ['aero','robot'],
+	ipb: p5
+};
+
+var Etp6 ={
+	nom: 'CS Commu',
+	lien: 'https://www.ingenib.fr/students/catalogue/diginext',
+	filiereRecherchee: ['mmk','elec'],
+	activite: ['aero','robot'],
+	ipb: p6
+};
+
+var Etp7 ={
+	nom: 'NXP',
+	lien: 'https://www.ingenib.fr/students/catalogue/diginext',
+	filiereRecherchee: ['mmk','elec'],
+	activite: ['aero','robot'],
+	ipb: p7
+};
+
+var Etp8 ={
+	nom: 'Thalès',
+	lien: 'https://www.ingenib.fr/students/catalogue/diginext',
+	filiereRecherchee: ['mmk','elec','telecom','info'],
+	activite: ['aero','robot'],
+	ipb: p8
+};
+
+var Etp9 ={
+	nom: 'Ubisoft',
+	lien: 'https://www.ingenib.fr/students/catalogue/diginext',
+	filiereRecherchee: ['telecom','info'],
+	activite: ['jeux','robot'],
+	ipb: p9
+};
+
+listeEtp=[Etp1,Etp2,Etp3,Etp4,Etp5,Etp6,Etp7,Etp8,Etp9];
